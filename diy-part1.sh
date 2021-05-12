e1450920ed9a1b0fa9147fe487bd1a8a6465d78c
bull13-GitHub <64085499+bull13-GitHub@users.noreply.github.com>
2021-05-12 14:09:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small.git package/openwrt-small
+git clone https://github.com/bull13-GitHub/small
